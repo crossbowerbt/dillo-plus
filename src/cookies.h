97a645291a612d1910227327a5164a28bc7bad1b
KuchiKuu <kuchikuu@cock.li>
2024-01-08 15:08:44 +0100

    Add a toggle to enable/disable cookies in the browser
    If Dillo is compiled with -DDISABLE_COOKIES then the menu
    entry is not compiled. If the user decides to include cookies
    then a menu item will show up, giving the user to disable
    the cookies for the current session.
    (The state is not saved anywhere)
    
    Two functions have been added to cookies.c to check and set
    the "disabled" variable (thus enabling/blocking cookies)
    
    menu.cc looks a bit chaotic, but should be fine (?)

--- src/cookies.h
+++ src/cookies.h
@@ -11,12 +11,18 @@ void  a_Cookies_init( void );
 # define a_Cookies_get_query(url, requester)  dStrdup("")
 # define a_Cookies_set()     ;
 # define a_Cookies_freeall() ;
+# define a_Cookies_get_disabled() ;
+# define a_Cookies_set_disabled(bool_t) ;
 #else
   char *a_Cookies_get_query(const DilloUrl *query_url,
                             const DilloUrl *requester);
   void  a_Cookies_set(Dlist *cookie_string, const DilloUrl *set_url,
                       const char *server_date);
   void  a_Cookies_freeall( void );
+
+  bool_t  a_Cookies_get_disabled( void );
+
+  void  a_Cookies_set_disabled( bool_t new_state );
 #endif
 
 
